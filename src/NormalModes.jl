5fb7dae9a3463081e36fa12752d3bd75b6d9011e
Benoît Richard <benoit.richard@desy.de>
2023-05-31 18:27:38 +0200

    Document functions

--- src/NormalModes.jl
+++ src/NormalModes.jl
@@ -19,9 +19,19 @@ struct NormalDecomposition{T}
     U::Matrix{T}  # Orthonormal modes
 end
 
-function NormalDecomposition(hessian::Matrix, masses::Vector ; valid_modes = 1:3length(masses))
+"""
+    NormalDecomposition(hessian, elements)
+
+Compute the normal mode decomposition of the `3n x 3n` Hessian for a system
+composed of atoms of the given elements, where `n` is the number of atoms.
+
+The elements can be given as either elements from PeriodicTable, by their
+atomic number or by their mass, defaulting to atomic units when not usng a
+Unitful quantity.
+"""
+function NormalDecomposition(hessian::Matrix, elements::Vector ; valid_modes = 1:3length(elements))
     hessian = to_atomic_units(hessian)
-    masses = to_atomic_masses(masses)
+    masses = to_atomic_masses(elements)
 
     m = @chain masses begin
         vcat([fill(m, 3) for m in _]...)
@@ -51,11 +61,21 @@ to_atomic_masses(x) = to_atomic_units(x)
 to_atomic_masses(x::VecOrMat{<:Element}) = [austrip(elem.atomic_mass) for elem in x]
 to_atomic_masses(x::VecOrMat{<:Integer}) = to_atomic_masses([elements[i] for i in x])
 
+"""
+    project(nm::NormalDecomposition, v)
+
+Project the given geometries (3 x n_atoms x n_obs) on the normal modes.
+"""
 function project(nm::NormalDecomposition, v::Array{<:Any, 3})
     projector = nm.U' * Diagonal(1 ./ nm.m)
     return projector * reshape(v, length(nm.m), :)
 end
 
+"""
+    normal_modes(nm::NormalDecomposition)
+
+Real space normal modes according to a normal decomposition.
+"""
 function normal_modes(nm::NormalDecomposition)
     modes = Diagonal(nm.m) * nm.U
     μs = norm.(eachcol(modes))
@@ -74,32 +94,29 @@ function reduced_masses(nm::NormalDecomposition)
     end
 end
 
-frequencies(nm::NormalDecomposition) = @chain nm.ωs begin
-    _ ./ 2π 
-    _ * aunit(u"s^-1")  # Add atomic unit of inverse time
-    uconvert.(u"GHz", _)  # Standard units
+function frequencies(nm::NormalDecomposition)
+    @chain nm.ωs begin
+        _ ./ 2π 
+        _ * aunit(u"s^-1")  # Add atomic unit of inverse time
+        uconvert.(u"GHz", _)  # Standard units
+    end
 end
 
-wave_number(nm::NormalDecomposition) = @chain nm begin
-    frequencies(_)
-    _ ./ 1u"c"  # Divide by the speed of light
-    uconvert.(u"cm^-1", _)  # Standard units
+function wave_number(nm::NormalDecomposition)
+    @chain nm begin
+        frequencies(_)
+        _ ./ 1u"c"  # Divide by the speed of light
+        uconvert.(u"cm^-1", _)  # Standard units
+    end
 end
 
-# TODO Not sure this is correct
-function position_covariance(nm::NormalDecomposition ; regularize = true)
-    modes = normal_modes(nm)
-    rawΣ = modes * Diagonal(nm.ωs) * modes'
+"""
+    sample(nm::NormalDecomposition, n_samples)
 
-    !regularize && return Symetric(rawΣ)
+Perform Wigner sampling according to the normal decomposition.
 
-    evals, evecs = eigen(rawΣ ; sortby = abs)
-    evals[1:6] .= evals[7] * 1e-15
-
-    return Symmetric(evecs * Diagonal(evals) * evecs')
-end
-
-# TODO Extend to non-atomic units
+Return the deviation from the average geometry.
+"""
 function StatsBase.sample(nm::NormalDecomposition, n_samples)
     hbar = 1  # Atomic units
     m = 1  # All modes have mass 1
